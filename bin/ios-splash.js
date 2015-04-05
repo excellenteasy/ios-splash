73ddf15a46b1646b6f3d3e43e06f873e747f415e
David Pfahler <david@excellenteasy.com>
2015-04-05 14:53:29 +0200

    feat: abbreviated flags
    BREAKING CHANGE: --version no longer returns a version number. First of all, semantic-release goes with removing the version number from your package.json. But also, the version number really shouldn't matter.
    
    Furthermore, you can now use all flags in an abbreviated manner as long as they are unambiguous. For example:
    `ios-splash --wi 320` works just like `ios-splash --width 320`. This works for all flag, but, for example, not for
    `ios-splash --he 480` because `--he` could be both `--help` or `--height`.

--- bin/ios-splash.js
+++ bin/ios-splash.js
@@ -1,32 +1,50 @@
 #!/usr/bin/env node
 'use strict'
-var minimist = require('minimist')
-var pkg = require('../package.json')
+var abbrev = require('abbrev')
+var argv = require('yargs')
 var splash = require('../')
 
-var argv = minimist(process.argv.slice(2))
+// help
+argv.help('help')
+argv.alias('h', 'help')
 
-function help () {
-  console.log([
-    pkg.description,
-    '',
-    'Use `--format json` to set output to JSON.',
-    'Get specifc splash by width, height or name by using `--width`, `--height` or `--size`.',
-    '',
-    'Examples:',
-    '  $ ios-splash --width 320',
-    '  Default~iphone.png,320,480',
-    '',
-    '  $ ios-splash --size "~iphone" --format json',
-    '  {"name":"Default~iphone.png","width":320,"height":480}'
-  ].join('\n'))
-}
+// register abbreviated aliases
+var abbrevs = abbrev(['width', 'height', 'size', 'format', 'help'])
+var aliases = Object.keys(abbrevs)
+aliases.forEach(function (alias) {
+  if (alias !== abbrevs[alias]) {
+    argv.alias(alias, abbrevs[alias])
+  }
+})
+
+// document options
+argv.option('width', {
+  description: 'width of the image in pixels'
+})
+argv.option('height', {
+  description: 'height of the image in pixels'
+})
+argv.option('size', {
+  description: 'string identifiying the splash image'
+})
+argv.option('format', {
+  description: 'format of the output to stdout (csv or json)'
+})
+
+argv.usage('Usage: ios-splash [options]')
+
+argv.example('$ ios-splash --width 320', 'Default~iphone.png,320,480')
+argv.example('$ ios-splash --width 320 --format json', '{"name":"Default~iphone.png","width":320,"height":480}')
+argv.example('$ ios-splash --size ~iphone', 'Default~iphone.png,320,480')
+
+argv = argv.argv
 
 function formatLog (splash, argv) {
-  var format = (argv.format || 'csv').toLowerCase()
+  var format = argv.format
   if (format === 'json') {
     return JSON.stringify(splash)
   }
+  if (!(format === 'csv' || format === 'json')) format = 'csv'
   if (!Array.isArray(splash)) {
     splash = [splash]
   }
@@ -36,14 +54,10 @@ function formatLog (splash, argv) {
 }
 
 function cli () {
-  if (argv.help || argv.h) return help()
-
-  if (argv.version || argv.v) return console.log(pkg.version)
-
   var options = {
-    size: argv.size || argv.s,
-    width: argv.width || argv.w,
-    height: argv.height || argv.h
+    size: argv.size,
+    width: argv.width,
+    height: argv.height
   }
 
   var output = splash(options)
